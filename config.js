1d723b68c88677a9e7b88359e34a1856184004c4
ggichuru <ggichuru.dev@gmail.com>
2021-12-05 00:55:01 +0300

    Hardhat setup

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const settings = {
+    kovan_url: process.env.KOVAN_URL,
+    etherscan_key: process.env.ETHERSCAN_API_KEY,
+    ac_pk: process.env.AC_PK
+}
+
+export default settings
---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+const { default: settings } = require("./config");
+
+require("@nomiclabs/hardhat-waffle");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    kovan: {
+      chainId: 42,
+      accounts: [settings.ac_pk],
+      url: settings.kovan_url
+    }
+  }
+};
